e5463001c28b3536935d1535437270912929dc69
LLL-zqvr <13657794496@163.com>
2024-06-23 17:25:17 +0800

    Initial commit

---
+++ src/main/java/com/example/controller/LoginServlet.java
@@ -0,0 +1,36 @@
+package com.example.controller;
+
+import com.example.entity.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            //转发
+        req.getRequestDispatcher("/WEB-INF/jsp/login.html")
+                .forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String account = req.getParameter("account");
+        String password = req.getParameter("password");
+
+        String url;
+        if("9376".equals(account) && "123456".equals(password)){
+            User u = new User("liu");
+            req.getSession().setAttribute("user",u);
+            url = "/welcome";
+        }else {
+            url = "/login";
+        }
+        resp.sendRedirect(req.getContextPath()+url);
+    }
+}
---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.entity;
+
+public class User {
+    String userName;
+    String account;
+    String password;
+
+    public User(String userName) {
+        this.userName = userName;
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
