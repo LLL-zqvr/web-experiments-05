17aea12b5023320f260e0050053456b2b7bb4e22
LLL-zqvr <13657794496@163.com>
2024-07-03 22:28:22 +0800

    init

--- src/main/java/com/controller/LoginServlet.java
+++ src/main/java/com/controller/LoginServlet.java
@@ -9,18 +9,20 @@ import java.io.IOException;
 
 @WebServlet("/filter/login")
 public class LoginServlet extends HttpServlet {
+    private static final String BASE = "/WEB-INF/jsp/";
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            //转发
-        req.getRequestDispatcher("/WEB-INF/jsp/login.html")
-                .forward(req,resp);
+            //RequestDispatcher。请求转发,将客户端的请求发送至服务器内任何其他资源(servlet/HTML/JSP)
+        System.out.println("5");
+        req.getRequestDispatcher(BASE + "login.html")//创建一个封装了指定资源的RequestDispatcher对象
+                .forward(req,resp);//传递request/response对象
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String account = req.getParameter("account");
         String password = req.getParameter("password");
-
+        System.out.println("6");
         String url;
         if("9376".equals(account) && "12345".equals(password)){
             User u = new User("liu");
@@ -29,6 +31,8 @@ public class LoginServlet extends HttpServlet {
         }else {
             url = "/filter/login";
         }
+        System.out.println("7");
         resp.sendRedirect(req.getContextPath()+url);
+        //System.out.println(req.getContextPath()+url);
     }
 }
--- src/main/java/com/filter/LoginFilter.java
+++ src/main/java/com/filter/LoginFilter.java
@@ -17,21 +17,29 @@ public class LoginFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //排除路径
+        System.out.println("1");
         for(String e : excludes) {
+
             //如果相等，既是登录页面的话直接就放过去
             if(e.equals(req.getServletPath())) {
+                //System.out.println(e +" " + req.getServletPath());
+                System.out.println("2");
                 chain.doFilter(req,res);
                 return;
             }
         }
+
         User user = (User) req.getSession().getAttribute("user");
         if(user != null){
             chain.doFilter(req,res);
         }
             //重定向
             //request.getContextPath()获取的是项目名
+        //System.out.println("3");
             res.sendRedirect(req.getContextPath() + "/filter/login");
-            //System.out.println(req.getContextPath() + "/login");
+//        System.out.println();
+            //System.out.println(req.getContextPath());
+        //System.out.println(req.getServletPath());
 
     }
 }
